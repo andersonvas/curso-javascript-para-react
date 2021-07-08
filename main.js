c9180bd577fd35eaeb7161c347263d4f2fa07ab6
AP - EA Anderson de Carvalho Vasconcelos <anderson.vasconcelos@ucb.org.br>
2021-07-08 16:37:41 -0300

    :art: style cardgamer and new component player

--- main.js
+++ main.js
@@ -7,7 +7,5 @@ import CardGame from "./src/components/CardGame";
 const $root = document.querySelector("#root");
 const $htmlCardGame = CardGame();
 
-console.log($htmlCardGame);
-
 $root.insertAdjacentHTML("beforeend", $htmlCardGame);
 
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,9 +2,29 @@ import './style.css';
 
 function CardGame() {
     return `
-        <article class="card-game">
-            <img src="images/alura-pixel.png" alt="Logo da Alura" />
-        </article>
+        <div class="background">
+            <article class="card-game" id="card1">
+                <img src="images/alura-pixel.png" alt="Logo da Alura" />
+            </article>
+            <article class="card-game hide" id="card1-flip">
+                <img src="images/alura-pixel.png" alt="Logo da Alura" />
+            </article>
+            <article class="card-game">
+                <img src="images/alura-pixel.png" alt="Logo da Alura" />
+            </article>
+            <article class="card-game">
+                <img src="images/alura-pixel.png" alt="Logo da Alura" />
+            </article>
+            <article class="card-game">
+                <img src="images/alura-pixel.png" alt="Logo da Alura" />
+            </article>
+            <article class="card-game">
+                <img src="images/alura-pixel.png" alt="Logo da Alura" />
+            </article>
+            <article class="card-game">
+                <img src="images/alura-pixel.png" alt="Logo da Alura" />
+            </article>
+        </div>
     `
 }
 
