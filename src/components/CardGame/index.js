8108d3b9c8b354c8192159887a63ca9815f4401b
Anderson <anderson.vasco15@gmail.com>
2021-07-11 00:12:42 -0300

    :construction: Create BoardGame object without css

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function CardGame() {
+    return `
+        <article class="card-game" id="card1">
+            <img src="images/alura-pixel.png" alt="Logo da Alura" />
+        </article>
+    `
+}
+
+export default CardGame;
---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,10 @@
+function PlayerScore(p1, p2) {
+    p1 = "Player1";
+    p2 = "Player2";
+
+    return `
+        <h2>$p1</h2>
+    `
+}
+
+export default PlayerScore;
